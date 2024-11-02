6974079648fb2993835b7237f8a8ad49a5024d83
AlanXiao <AlanXiao@Alans-MacBook-Pro-14.local>
2024-11-02 09:41:16 -0700

    Commit message

--- run_alert.sh
+++ run_alert.sh
@@ -2,4 +2,4 @@
 # Activate the virtual environment
 source /home/smby1985/forum-alert-env/bin/activate
 # Run the Python script
-python /home/smby1985/snipe_hide_alert.py >> /home/smby1985/log.txt 2>&1
+python /home/smby1985/personal/snipe_hide_alert.py >> /home/smby1985/personal/log.txt 2>&1
