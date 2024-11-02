a0b883f3fe78615472dc719c0a8a81f59ddaa58f
AlanXiao <AlanXiao@Alans-MacBook-Pro-14.local>
2024-11-02 14:35:08 -0700

    message

--- run_alert.sh
+++ run_alert.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-# Activate the virtual environment
+
+# Set the working directory
+cd /home/smby1985/personal
+
+# Add environment variables
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PYTHONPATH=/home/smby1985/personal
+
+# Activate the virtual environment with full path
 source /home/smby1985/personal/forum-alert-env/bin/activate
-# Run the Python script
-python /home/smby1985/personal/snipe_hide_alert.py >> /home/smby1985/personal/log.txt 2>&1
+
+# Run the Python script with full path and proper error logging
+python /home/smby1985/personal/snipe_hide_alert.py >> /home/smby1985/personal/cron_log.txt 2>&1
+
+# Deactivate virtual environment
+deactivate
