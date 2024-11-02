3256a627e8bc9e9c35e112717a81265a1a8e768e
AlanXiao <AlanXiao@Alans-MacBook-Pro-14.local>
2024-11-02 10:28:49 -0700

    message

--- run_alert.sh
+++ run_alert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Activate the virtual environment
-source /home/smby1985/forum-alert-env/bin/activate
+source /home/smby1985/personal/forum-alert-env/bin/activate
 # Run the Python script
 python /home/smby1985/personal/snipe_hide_alert.py >> /home/smby1985/personal/log.txt 2>&1
